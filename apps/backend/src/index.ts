731cfdd910c72a57f63b765805856619cc467ba6
Jagjeevan Kashid <jagjeevandev97@gmail.com>
2025-11-22 05:03:55 +0530

    refatured routes
    Signed-off-by: Jagjeevan Kashid <jagjeevandev97@gmail.com>

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,6 +1,7 @@
 import cookieParser from "cookie-parser";
 import express, { urlencoded } from "express";
 import cors from "cors";
+
 const app = express();
 
 app.use(cookieParser());
@@ -13,18 +14,10 @@ app.use(
   })
 );
 
-import authRouter from "./routes/auth.routes";
-import credRouter from "./routes/cred.routes";
-import triggerRouter from "./routes/triggers.routes"
-import workflowRouter from "./routes/workflow.routes"
-import webHookRouter from "./routes/webhook.routes"
+import v1 from "./routes"
 import { errorHandler } from "./middlewares/error.middleware";
 
-app.use("/api/v1/auth", authRouter);
-app.use("/api/v1/cred", credRouter);
-app.use("/api/v1/trigger", triggerRouter)
-app.use('/api/v1/workflow',workflowRouter )
-app.use('/api/v1/webhook', webHookRouter )
+app.use("/api/v1", v1);
 
 app.listen(8888, () => {
   console.log("app is listening on port ", 8888);
