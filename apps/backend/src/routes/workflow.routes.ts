ebb7f374dc941681cc070ac7344c1ac1431b52f1
Jagjeevan Kashid <jagjeevandev97@gmail.com>
2025-12-02 10:18:35 +0530

    fix: added execution routes
    Signed-off-by: Jagjeevan Kashid <jagjeevandev97@gmail.com>

--- apps/backend/src/routes/workflow.routes.ts
+++ apps/backend/src/routes/workflow.routes.ts
@@ -1,15 +1,24 @@
 import { Router } from "express";
-import { 
-  saveWorkflow, 
-  getWorkflowById, 
-  updateWorkflow, 
-  getUserWorkflows, 
+import {
+  saveWorkflow,
+  getWorkflowById,
+  updateWorkflow,
+  getUserWorkflows,
   executeFlow,
   deleteWorkflow
 } from "../controllers/workflow.controller";
+import {
+  getUserExecutions,
+  getExecutionDetails,
+  getExecutionStats
+} from "../controllers/execution.controller";
 import { isLoggedIn } from "../middlewares/auth.middleware";
 
 const router = Router();
+
+router.get('/executions/list', isLoggedIn, getUserExecutions);
+router.get('/executions/stats', isLoggedIn, getExecutionStats);
+router.get('/executions/:executionId', isLoggedIn, getExecutionDetails);
 
 router.post('/save', isLoggedIn, saveWorkflow);
 router.get('/:workflowId', isLoggedIn, getWorkflowById);
